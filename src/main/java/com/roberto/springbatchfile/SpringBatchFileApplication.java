375c8f033118f1fcb973a07491b80f062d6ac9e6
roberto <rvneto.rs@gmail.com>
2024-04-22 19:32:34 -0300

    Commit inicial

---
+++ src/main/java/com/roberto/springbatchfile/SpringBatchFileApplication.java
@@ -0,0 +1,13 @@
+package com.roberto.springbatchfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchFileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roberto/springbatchfile/SpringBatchFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roberto.springbatchfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
